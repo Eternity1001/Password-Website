0fb13f35b92a1df47c9dcd7e131202714251e28d
Eternity1001 <MMirhossain01@gmail.com>
2026-08-24 20:28:13 -0400

    added the main files needed to start the project

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def signin():
+    if request.method == "POST":
+        username_or_email = request.form.get("username_or_email")
+        password = request.form.get("password")
+
+        print(username_or_email)
+        print(password)
+
+        return "Sign in successful!"
+
+    return render_template("signin.html")
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        username_or_email = request.form.get("username_or_email")
+        password = request.form.get("password")
+
+        print(username_or_email)
+        print(password)
+
+        return "Sign in successful!"
+
+    return render_template("signup.html")
+
+@app.route("/forgot-username")
+def forgot_username():
+    return "Forgot username"
+
+
+@app.route("/forgot-password")
+def forgot_password():
+    return "Forgot password"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
